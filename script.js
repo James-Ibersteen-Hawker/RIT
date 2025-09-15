718e4efbc845c008e0eecf8c90a47439e2925b61
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-09-15 10:33:45 -0400

    made nav selection

---
+++ script.js
@@ -0,0 +1,26 @@
+const NAV = {
+  src: document.querySelector("header"),
+  links: Array.from(document.querySelectorAll(".link")).map(
+    (e) =>
+      new Proxy([e, false], {
+        set(t, p, v) {
+          if (Number(p) === 1) {
+            let reflect = Reflect.set(t, p, v);
+            if (t[p] === false) t[0].classList.remove("active");
+            else if (t[p] === true) t[0].classList.add("active");
+            return reflect;
+          }
+        },
+      })
+  ),
+  init() {
+    this.links.forEach((e, i) => {
+      function clickFunction() {
+        this.links.forEach((e) => (e[1] = false));
+        e[1] = true;
+      }
+      e[0].addEventListener("click", clickFunction.bind(this));
+    });
+  },
+};
+NAV.init();
